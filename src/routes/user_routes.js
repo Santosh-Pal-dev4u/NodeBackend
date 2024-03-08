f540e7e4a10f7a19d472bdc6e56a48dc644ddad1
Santosh Pal <santoshbhagel321@gmail.com>
2024-03-08 18:25:21 +0530

    creating user

--- src/routes/user_routes.js
+++ src/routes/user_routes.js
@@ -1,10 +1,14 @@
 import { Router } from "express";
 import { registerUser } from "../controllers/user_controller.js";
+import { upload } from "../middlewares/multer_middleware.js";
 
 const router = Router();
 
-router.post("/register", registerUser);
-router.get("/register", registerUser);
+router.post("/register", upload.fields([
+    { name: "avatar", maxCount: 1 },
+    { name: "cover", maxCount: 1 }
+]), registerUser);
+
 // router.route("/register").post(registerUser)
 
 export default router;
